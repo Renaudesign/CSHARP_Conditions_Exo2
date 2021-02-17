91aff61f320c7530f32a165c289be857f885e866
renaudesign <renaud.sourmail@gmail.com>
2021-02-17 09:17:14 +0100

    exercice 2 partie 2 terminé

---
+++ CSHARP_Conditions_Exo2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace CSHARP_Conditions_Exo2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bonjour sujet n°2 035 654. Veuillez nous indiquer vôtre âge en nombre de cycles solaires terrestres standard.");
+            int age = int.Parse(Console.ReadLine());
+
+            if (age >= 18)
+            {
+                Console.WriteLine($"Sujet n°2 035 654, vous avez {age} ans. Vous êtes donc majeur, veuillez vous orienter vers le service des assignations professionelles.");
+            }
+            else
+            {
+                Console.WriteLine($"Sujet n°2 035 654, vous avez {age} ans. Vous n'êtes donc pas majeur, veuillez retourner dans votre cycle de formation.");
+            };
+
+        }
+    }
+}
